e1ff15116e56ba2094aac0d7c579f2fedc501753
Hardik Dua <hardikdua08@gmail.com>
2020-04-14 15:19:28 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/hardik/covidindiatracker/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.hardik.covidindiatracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    companion object {
+        private var SPLASH_SCREEN_TIME_OUT : Long = 2000
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
+        val runnable = Runnable {
+            val mainActivity = Intent(applicationContext, MainActivity::class.java)
+            startActivity(mainActivity)
+            finish()
+        }
+        Handler().postDelayed(runnable, SPLASH_SCREEN_TIME_OUT)
+    }
+}
